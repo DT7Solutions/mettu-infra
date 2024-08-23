5e249a3eac4f5c89f3e825ddd630d689589d5470
website-krysta <website.krysta@gmail.com>
2024-08-23 18:22:22 +0530

    fixed all layout designs

--- assets/js/main.js
+++ assets/js/main.js
@@ -360,6 +360,38 @@
             }
         });
 
+    /* ==================================================
+            # project Carousel
+         ===============================================*/
+         $('.projects-carousel').owlCarousel({
+            loop: false,
+            margin: 30,
+            nav: false,
+            navText: [
+                "<i class='fa fa-angle-left'></i>",
+                "<i class='fa fa-angle-right'></i>"
+            ],
+            dots: false,
+            autoplay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+            }
+        });
+
+
+        /* ==================================================
+            # Services Carousel 3 Colum
+         ===============================================*/
+
+
 
         /* ==================================================
             # Blog Carousel
